7a8299f8757a31fcc43b4be61ffe1ee7bcb7661d
Kevin Axel Tagliaferri <kevinaxeltagliaferri@gmail.com>
2024-12-18 15:44:35 -0300

    Changing support msg. and links at README.md.

--- README.md
+++ README.md
@@ -22,3 +22,13 @@ acad.model.AddLine(p0(), p1())
 # Zoom the line by extents.
 acad.app.ZoomExtents()
 ```
+
+
+## Links
+
+- Documentation at: [ReadTheDocs](https://pyacad.readthedocs.io/en/latest/)
+- Source code and issue tracking at: [GitHub](https://pypi.org/project/pyacad/)
+
+## Support me:
+
+- If you want to pay me for a beer, coffee, or something else: [C|_|] (https://www.paypal.com/paypalme/KevinAxelTagliaferri)
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 install_requires = []
 
 if platform.system() == "Windows":
-      install_requires.append("pywin32")
+    install_requires.append("pywin32")
 
 readme = open("./README.md", "r")
 
@@ -12,7 +12,7 @@ setup(name="pyacad",
       packages=["pyacad"],
       version="0.0.5",
       description="A package aimed to simplfy coding of Activex Automation Module of AutoCAD, based on pywin32 library.",
-      long_description = readme.read(),
+      long_description=readme.read(),
       long_description_content_type="text/markdown",
       author="Kevin Axel Tagliaferri",
       author_email='kevinaxeltagliaferri@hotmail.com',
